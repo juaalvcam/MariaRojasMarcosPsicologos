a88e8f0b311bcc7a7b0a4988a2ea1b712a5202ff
rubsuadav <rsuarezdavid@gmail.com>
2024-01-25 16:27:41 +0100

    Add about page and update social media links in base.html

--- psicoMRM/views.py
+++ psicoMRM/views.py
@@ -7,3 +7,7 @@ from .scrapping import populate
 def index(request):
     populate()
     return render(request, "index.html", {"opiniones": Opiniones.objects.all().order_by("-fecha")[:6]})
+
+
+def about(request):
+    return render(request, "about.html")
