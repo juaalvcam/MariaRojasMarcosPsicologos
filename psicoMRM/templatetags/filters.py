359a353e01383ad4bdc9d1720ff0a977fa01f58e
rubsuadav <rsuarezdavid@gmail.com>
2024-01-29 20:13:06 +0100

    Add static file loading and remove_static filter

--- psicoMRM/templatetags/filters.py
+++ psicoMRM/templatetags/filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 @register.filter(name='addcss')
 def addcss(field, css):
     return field.as_widget(attrs={"class": css})
+
+
+@register.filter
+def remove_static(value):
+    return value.url.replace('/static', '', 1)
