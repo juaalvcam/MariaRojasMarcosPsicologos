0a858e594d752776720e60637e43828d4fa269be
rubsuadav <rsuarezdavid@gmail.com>
2024-01-29 12:04:05 +0100

    Update blog template and views.py

--- psicoMRM/views.py
+++ psicoMRM/views.py
@@ -91,22 +91,20 @@ def logout_session(request):
 # BLOG
 def blog(request):
     form = BlogForm()
-    lista = []  # lista para ir añadiendo los artículos
     articulos = Articulo.objects.all()
     if request.method == "POST":
         form = BlogForm(request.POST, request.FILES)
         if form.is_valid():
             imagen = form.cleaned_data["imagen"]
             try:
-                articulo = Articulo.objects.create(
+                Articulo.objects.create(
                     titulo=form.cleaned_data["titulo"],
                     descripcion=form.cleaned_data["descripcion"],
                     imagen=imagen
                 )
-                lista.append(articulo)
             except IntegrityError:
                 form.add_error('titulo', 'Este título ya existe.')
-                return render(request, "blog.html", {"articulos": lista, "form": form, 'STATIC_URL': settings.STATIC_URL})
+                return render(request, "blog.html", {"articulos": articulos, "form": form, 'STATIC_URL': settings.STATIC_URL})
         return render(request, "blog.html", {"articulos": articulos, "form": form, 'STATIC_URL': settings.STATIC_URL})
     else:
         return render(request, "blog.html", {"articulos": articulos, "form": form, 'STATIC_URL': settings.STATIC_URL})
